17ec579a306df22481bf4ac291a7f09767694cd2
Shoaib Ahmad <79258797+shoaibahmadse@users.noreply.github.com>
2023-07-31 01:03:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, Form
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
 app = FastAPI()
 
@@ -9,7 +12,18 @@ async def jd(job_title: str = Form(...), vacancy: str = Form(...), gender: str =
 
     # Print the values just for debugging purposes (you can remove this if you don't need it).
     print(job_title, job_experience, vacancy, gender)
+    jd=job_description(job_title, job_experience, vacancy, gender)
 
     # You can perform any processing logic here if required.
     # For now, we'll simply return "Hello, World!" as the response.
-    return {'Job Title':job_title,"Job Experience":job_experience,"Vacancy":vacancy,"Gender":gender}
+    return {'Job Description':jd}
+
+
+def job_description(job_title,vacancy,gender,exprience):
+    llm = OpenAI(temperature=0, max_tokens=1000, openai_api_key="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")
+    template = """i have vacancy of {vacancy} person for a job {job_title} which is a {gender} vanacy and required job exprience is {exprience}. Give me a proper description of this which include : Description of the job , Responsibilites, Requirements.Note: Does not include any of the additional text. 
+                """
+    prompt_template = PromptTemplate(input_variables=["job_title","vacancy","gender","exprience"], template=template)
+    answer_chain = LLMChain(llm=llm, prompt=prompt_template)
+    answer = answer_chain.run({'job_title': job_title, 'vacancy': vacancy,'exprience':exprience,'gender':gender})
+    return answer
